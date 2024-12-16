799edb583523c8303bce2340160840f79c1a688b
Kamil Malecek <kamil.malecek@bearingpoint.com>
2024-12-16 13:59:51 +0100

    indentation, camel case

--- actions/lastSuccessfulCommit/index.js
+++ actions/lastSuccessfulCommit/index.js
@@ -3,18 +3,18 @@ const github = require('@actions/github');
 
 // inspired by https://github.com/simondotm/last-successful-commit-action
 try {
-    const octokit = github.getOctokit(core.getInput('github_token'));
+    const octokit = github.getOctokit(core.getInput('githubToken'));
     const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
     const params = {
         owner,
         repo,
-        workflow_id: core.getInput('workflow_id'),
+        workflow_id: core.getInput('workflowId'),
         status: 'success',
         branch: core.getInput('branch')
     }
     // optionally filter workflow runs by the event that triggered them
     // unlike v1, there is no longer a default event of 'push', the default event type is 'any'
-    const event = core.getInput('workflow_event')
+    const event = core.getInput('workflowEvent')
     if (event) {
         params.event = event
     }
@@ -25,7 +25,7 @@ try {
                 res.data.workflow_runs.length > 0
                     ? res.data.workflow_runs[0].head_commit.id
                     : '';
-            core.setOutput('commit_hash', lastSuccessCommitHash);
+            core.setOutput('commitHash', lastSuccessCommitHash);
         })
         .catch((e) => {
             core.setFailed(e.message);
